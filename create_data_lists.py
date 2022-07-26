89d314d820da606dd43fd9cc053e949834c77d40
JYeonKim <juyeonk.kor@gmail.com>
2022-07-26 12:05:08 +0000

    rgb_baseline_수정

--- create_data_lists.py
+++
@@ -1,6 +0,0 @@
-from utils import create_data_lists
-
-if __name__ == '__main__':
-    print("시작2")
-    create_data_lists(kaist_path='/content/drive/MyDrive/KAIST_PD',
-                      output_folder='/content/drive/MyDrive/kaist_output')
