4d551027954806b1001dd82bde150262d38ed707
JYeonKim <juyeonk.kor@gmail.com>
2022-07-27 03:55:23 +0000

    rgb_epoch_100

--- create_data_lists (1).py
+++
@@ -1,5 +0,0 @@
-# from utils import create_data_lists
-
-# if __name__ == '__main__':
-#     create_data_lists(kaist_path='/media/ssd/ssd data/VOC2012',
-#                       output_folder='/content/drive/MyDrive/kaist_output/')
